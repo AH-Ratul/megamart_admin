c6c73b8084f8c14ce99c726afecb5b0403a303f0
AH-Ratul <ahratul21@gmail.com>
2024-10-01 18:48:58 +0600

    design updated

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import SideBar from '../components/SideBar/SideBar';
-import MainPage from '../components/Home/MainPage';
+import React from "react";
+import SideBar from "../components/SideBar/SideBar";
+import MainPage from "../components/Home/MainPage";
 
 const Main = () => {
-    return (
-        <main className='bg-raisinBlack text-coolGray font-OpenSans flex '>
-            <SideBar />
-            <MainPage />
-        </main>
-    );
+  return (
+    <main className="bg-[#DFF3E4] text-[#2F2F2F] font-LeagueGothic flex ">
+      <div className="top-0 bg-white/90  rounded-tr-2xl rounded-br-2xl left-0 fixed w-64">
+        <SideBar />
+      </div>
+
+      <div className="ml-64 w-full overflow-y-auto overflow-x-hidden  h-screen">
+        <MainPage />
+      </div>
+    </main>
+  );
 };
 
 export default Main;
