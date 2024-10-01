6908a905f61b030ce05c910321d6552dece831a0
AH-Ratul <ahratul21@gmail.com>
2024-10-01 18:51:36 +0600

    some updates

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import logo from "../../../public/logo123.svg";
 
 const Login = () => {
   const {
@@ -19,16 +18,12 @@ const Login = () => {
 
   return (
     <main className="bg-raisinBlack text-white/70 font-OpenSans min-h-screen flex flex-col justify-center items-center">
-      <img src={logo} alt="logo" className="mb-12" />
-      <div className="w-full max-w-md p-6 border border-white/15 rounded-sm">
+      <div className="w-full max-w-sm p-6 border-2 border-white/15 rounded-lg">
         {/* header content */}
         <div>
-          <h2 className="text-xl mb-2 font-bold tracking-widest">
+          <h2 className="text-xl mb-2  tracking-wider">
             Admin Login
           </h2>
-          <p className="text-sm">
-            Enter your email below to login to your account
-          </p>
         </div>
         {/* main content */}
         <div className="mt-6">
@@ -41,8 +36,8 @@ const Login = () => {
                 type="email"
                 id="email"
                 name="email"
-                placeholder="Enter your email"
-                className="mt-2 w-full block ps-2 py-2 text-sm bg-raisinBlack border border-white/15 rounded"
+                placeholder="example@email.com"
+                className="mt-2 w-full block ps-4 py-2 text-sm bg-raisinBlack border border-white/15 rounded"
                 {...register("email", {
                   required: "Email is required",
                   pattern: {
@@ -59,7 +54,7 @@ const Login = () => {
             </div>
             <div>
               <div className="flex justify-between items-center mt-5 mb-2">
-                <label htmlFor="password" className="text-sm">
+                <label htmlFor="password" className="text-sm ">
                   Password
                 </label>
                 <Link className="text-sm underline">Forget Your Password?</Link>
@@ -68,8 +63,8 @@ const Login = () => {
                 type="password"
                 id="password"
                 name="password"
-                placeholder="Enter your password"
-                className="mt-1 w-full block ps-2 py-2 text-sm bg-raisinBlack border border-white/15 rounded"
+                placeholder="xxxxxxx"
+                className="mt-1 w-full block ps-4 py-2 text-sm bg-raisinBlack border border-white/15 rounded"
                 {...register("password", {
                   required: "Password is required",
                   minLength: 6,
@@ -84,7 +79,7 @@ const Login = () => {
             </div>
             <button
               type="submit"
-              className="w-full bg-white/65 hover:opacity-75 hover:transform hover:transition hover:ease-in-out hover:duration-300 text-black mt-4 rounded-md py-2 font-medium"
+              className="w-full text-sm  bg-prime hover:opacity-85 hover:transform hover:transition hover:ease-in-out hover:duration-300 text-black mt-4 rounded-md py-[10px] font-medium"
             >
               Login
             </button>
@@ -93,7 +88,7 @@ const Login = () => {
 
         <p className="text-sm text-center mt-7">
           Don't have an account?{" "}
-          <Link to="/signup" className="underline">
+          <Link to="/signup" className="underline text-prime  tracking-wide">
             Signup
           </Link>
         </p>
