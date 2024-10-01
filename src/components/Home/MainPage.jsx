2a51b8fd771ae1af384108019d6ea2c5de9a0504
AH-Ratul <ahratul21@gmail.com>
2024-10-01 18:49:53 +0600

    design updated

--- src/components/Home/MainPage.jsx
+++ src/components/Home/MainPage.jsx
@@ -5,27 +5,38 @@ import allIcons from "../../data/all-icons";
 const MainPage = () => {
   const { user } = allIcons;
   return (
-    <main className="w-[100dvw]">
-      <header className="flex justify-between items-center border-b border-b-white/15 py-2 px-5 ">
-        <div className="w-full flex-1">
+    <main>
+      <header className="flex gap-20 items-center  py-2 px-5 ">
+        <div>
+          <h1 className="text-2xl font-bold font-LeagueGothic text-prime">
+            DASHBOARD
+          </h1>
+        </div>
+        <div className="w-96 ">
           <form>
             <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Search className="absolute left-2.5 top-2.5 h-4 w-4 z-10 text-muted-foreground" />
               <input
                 type="search"
                 placeholder="Search products..."
-                className="border border-white/15 rounded-md py-2 px-3 text-sm focus:outline-none focus:ring-1 focus:ring-prime w-full appearance-none bg-[#202334] pl-8 shadow-none md:w-2/3 lg:w-1/3"
+                className="border border-gray-300 bg-white/90 backdrop-blur-md rounded-xl py-2 px-3 text-sm  w-full 2xl:w-[500px] pl-8 shadow-sm text-gray-700  placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-prime"
               />
             </div>
           </form>
         </div>
-        <div className="text-xl border border-white/25 rounded-full p-2">
-          {user}
-        </div>
       </header>
-      <div className="bg-[#12141D]  p-3">
-        <div className="border border-white/15 bg-raisinBlack  w-44 h-20 py-6 text-center ">
-          Total: $33566
+      <div className="bg-[#12141D flex items-center gap-9 p-3 mt-[10px]">
+        <div className="rounded-md shadow-lg backdrop-blur-lg bg-green-500 w-64 h-28 flex flex-col justify-center items-center text-white text-2xl">
+          <p>Total Revenue</p>
+          <span>$55454543</span>
+        </div>
+        <div className="rounded-md shadow-lg backdrop-blur-lg bg-green-500 w-64 h-28 flex flex-col justify-center items-center text-white text-2xl">
+          <p>Total Revenue</p>
+          <span>$55454543</span>
+        </div>
+        <div className="rounded-md shadow-lg backdrop-blur-lg bg-green-500 w-64 h-28 flex flex-col justify-center items-center text-white text-2xl">
+          <p>Total Revenue</p>
+          <span>$55454543</span>
         </div>
       </div>
     </main>
