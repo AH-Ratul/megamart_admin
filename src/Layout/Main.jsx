a9cdf0c3b40e2cc7da35bd2e6f71558ac0b65a7b
AH-Ratul <ahratul21@gmail.com>
2024-10-17 01:59:44 +0600

    update colors & font

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,12 +4,12 @@ import MainPage from "../components/Home/MainPage";
 
 const Main = () => {
   return (
-    <main className="bg-[#DFF3E4] text-[#2F2F2F] font-LeagueGothic flex ">
-      <div className="top-0 bg-white/90  rounded-tr-2xl rounded-br-2xl left-0 fixed w-64">
+    <main className="bg-muted text-[#2F2F2F] font-LeagueGothic flex ">
+      <div className="top-0 bg-white left-0 fixed w-60">
         <SideBar />
       </div>
 
-      <div className="ml-64 w-full overflow-y-auto overflow-x-hidden  h-screen">
+      <div className="ml-60 w-full overflow-y-auto overflow-x-hidden  h-screen">
         <MainPage />
       </div>
     </main>
