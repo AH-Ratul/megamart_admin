868bb503960f6b5113705e5121f58950749d921c
AH-Ratul <ahratul21@gmail.com>
2024-10-17 02:01:37 +0600

    ui updates & add shadcn component

--- src/components/Home/MainPage.jsx
+++ src/components/Home/MainPage.jsx
@@ -1,17 +1,20 @@
 import { Search } from "lucide-react";
 import React from "react";
 import allIcons from "../../data/all-icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 const MainPage = () => {
   const { user } = allIcons;
   return (
     <main>
-      <header className="flex gap-20 items-center  py-2 px-5 ">
-        <div>
-          <h1 className="text-2xl font-bold font-LeagueGothic text-prime">
-            DASHBOARD
-          </h1>
-        </div>
+      <header className="flex justify-between items-center py-2 px-5 ">
         <div className="w-96 ">
           <form>
             <div className="relative">
@@ -24,20 +27,33 @@ const MainPage = () => {
             </div>
           </form>
         </div>
+        <div className="">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="border bg-white rounded-full p-2 text-xl">{user}</DropdownMenuTrigger>
+            <DropdownMenuContent cls>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </header>
-      <div className="bg-[#12141D flex items-center gap-9 p-3 mt-[10px]">
-        <div className="rounded-md shadow-lg backdrop-blur-lg bg-green-500 w-64 h-28 flex flex-col justify-center items-center text-white text-2xl">
+      <div className="bg-[#12141D flex items-center gap-9 ml-5  mt-[15px]">
+        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-64 h-28 flex flex-col justify-center items-center text-2xl">
           <p>Total Revenue</p>
           <span>$55454543</span>
         </div>
-        <div className="rounded-md shadow-lg backdrop-blur-lg bg-green-500 w-64 h-28 flex flex-col justify-center items-center text-white text-2xl">
+        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-64 h-28 flex flex-col justify-center items-center text-2xl">
           <p>Total Revenue</p>
           <span>$55454543</span>
         </div>
-        <div className="rounded-md shadow-lg backdrop-blur-lg bg-green-500 w-64 h-28 flex flex-col justify-center items-center text-white text-2xl">
+        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-64 h-28 flex flex-col justify-center items-center text-2xl">
           <p>Total Revenue</p>
           <span>$55454543</span>
         </div>
+        
       </div>
     </main>
   );
