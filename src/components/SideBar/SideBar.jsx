1b6654012a71049776bafc7571b4925cb3cac7f4
AH-Ratul <ahratul21@gmail.com>
2024-10-18 16:42:46 +0600

    adjust width, text & icon for large screen

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -9,25 +9,30 @@ const SideBar = () => {
     <aside className="pt-4 border-r h-[100dvh]">
       {/* logo */}
       <div className="pl-4">
-        <img src={logo} alt="logo" className="w-36" />
+        <img src={logo} alt="logo" className="w-36 2xl:w-44" />
       </div>
 
       {/* Links */}
       <div className="flex flex-col justify-start mx-3 text-base font-medium mt-8">
-        <Link className="ps-2 py-2 w-full hover:text-prime flex items-center gap-1 hover:bg-coolGray hover:bg-opacity-5 hover:rounded-sm">
-          {dashboard}Dashboard
+        <Link className="ps-2 py-2 w-full hover:text-prime flex items-center gap-1 hover:bg-accent hover:bg-opacity-5 hover:rounded-sm">
+          <span className="text-lg 2xl:text-xl">{dashboard}</span>
+          <span className="2xl:text-lg">Dashboard</span>
         </Link>
-        <Link className="ps-2 py-2 w-full hover:text-prime flex items-center gap-1 hover:bg-coolGray hover:bg-opacity-5 hover:rounded-sm">
-          {add}Add Product
+        <Link className="ps-2 py-2 w-full hover:text-prime flex items-center gap-1 hover:bg-accent hover:bg-opacity-5 hover:rounded-sm">
+          <span className="text-lg 2xl:text-2xl">{add}</span>
+          <span className="2xl:text-lg">Add Product</span>
         </Link>
-        <Link className="ps-2 py-2 w-full hover:text-prime flex items-center gap-1 hover:bg-coolGray hover:bg-opacity-5 hover:rounded-sm">
-          {product}Manage Products
+        <Link className="ps-2 py-2 w-full hover:text-prime flex items-center gap-1 hover:bg-accent hover:bg-opacity-5 hover:rounded-sm">
+          <span className="text-lg 2xl:text-2xl">{product}</span>
+          <span className="2xl:text-lg">Manage Products</span>
         </Link>
-        <Link className="ps-2 py-2 w-full hover:text-prime flex items-center gap-1 hover:bg-coolGray hover:bg-opacity-5 hover:rounded-sm">
-          {recipt}Orders
+        <Link className="ps-2 py-2 w-full hover:text-prime flex items-center gap-1 hover:bg-accent hover:bg-opacity-5 hover:rounded-sm">
+          <span className="text-lg 2xl:text-2xl">{recipt}</span>
+          <span className="2xl:text-lg">Orders</span>
         </Link>
-        <Link className="ps-2 py-2 w-full hover:text-prime flex items-center gap-1 hover:bg-coolGray hover:bg-opacity-5 hover:rounded-sm">
-          {invoice}Invoices
+        <Link className="ps-2 py-2 w-full hover:text-prime flex items-center gap-1 hover:bg-accent hover:rounded-sm">
+          <span className="text-lg 2xl:text-2xl">{invoice}</span>
+          <span className=" 2xl:text-lg">Invoices</span>
         </Link>
       </div>
     </aside>
