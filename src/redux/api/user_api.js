c7c11170226448640f3df3800ba2742be633ae1d
AH-Ratul <ahratul21@gmail.com>
2024-10-20 01:41:32 +0600

    implement axios interceptors

--- src/redux/api/user_api.js
+++ src/redux/api/user_api.js
@@ -1,10 +1,44 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi } from "@reduxjs/toolkit/query/react";
+import axios from "axios";
+
+const axiosSecure = axios.create({
+  baseURL: `${import.meta.env.VITE_baseurl}api/v1/`,
+  withCredentials: true,
+});
+
+const baseQuery = async ({ url, method, body }) => {
+  try {
+    const result = await axiosSecure({
+      url,
+      method,
+      data: body,
+    });
+    return { data: result.data };
+  } catch (error) {
+    const { response } = error;
+    if (response) {
+      return {
+        error: {
+          status: response?.status,
+          data: response.data,
+          message: response.statusText || "An error occured",
+        },
+      };
+    } else {
+      return {
+        error: {
+          status: null,
+          data: null,
+          message: error.message || "An unexpected error occured",
+        },
+      };
+    }
+  }
+};
 
 export const userApi = createApi({
   reducerPath: "userApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.VITE_baseurl}api/v1/`,
-  }),
+  baseQuery: baseQuery,
   tagTypes: ["auth"],
   endpoints: (builder) => ({
     createUser: builder.mutation({
