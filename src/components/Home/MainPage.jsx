1ea1d52d8a3a1b9e632796ea8d1d3a5fb617bed7
AH-Ratul <ahratul21@gmail.com>
2024-11-06 21:41:36 +0600

    responsive update, fix width for big screen

--- src/components/Home/MainPage.jsx
+++ src/components/Home/MainPage.jsx
@@ -58,41 +58,47 @@ const MainPage = () => {
           </DropdownMenu>
         </div>
       </header>
-      <div className="flex items-center gap-5 ml-5  mt-[15px]">
-        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-64 2xl:w-80 h-36 2xl:h-40 p-4 flex flex-col">
+      <div className="flex justify-center items-center gap-5 ml-5 mr-5 mt-[15px]">
+        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-full h-36 2xl:h-44 p-4 2xl:pl-7 flex flex-col">
           <p className="text-lg 2xl:text-xl text-muted-foreground">
             Total Revenue
           </p>
-          <span className="text-2xl 2xl:text-3xl font-bold my-2">
+          <span className="text-2xl 2xl:text-4xl font-bold my-2 2xl:my-5">
             &#2547; 554545
           </span>
-          <span className="text-xs 2xl:text-sm tracking-wider text-white bg-black w-fit rounded-full px-2">
+          <span className="text-xs 2xl:text-base tracking-wider text-white bg-black w-fit rounded-full px-2">
             +12%
           </span>
         </div>
-        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-64 2xl:w-80 h-36 2xl:h-40 p-4 flex flex-col">
+        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-full h-36 2xl:h-44 p-4 2xl:pl-7 flex flex-col">
           <p className="text-lg 2xl:text-xl text-muted-foreground">
             Total Orders
           </p>
-          <span className="text-2xl 2xl:text-3xl font-bold my-2">554</span>
+          <span className="text-2xl 2xl:text-4xl font-bold my-2 2xl:my-5">
+            554
+          </span>
           <span className="text-xs 2xl:text-sm tracking-wider text-white bg-black w-fit rounded-full px-2">
             +10%
           </span>
         </div>
-        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-64 2xl:w-80 h-36 2xl:h-40 p-4 flex flex-col">
+        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-full h-36 2xl:h-44 p-4 2xl:pl-7 flex flex-col">
           <p className="text-lg 2xl:text-xl text-muted-foreground">
             Total Users
           </p>
-          <span className="text-2xl 2xl:text-3xl font-bold my-2">53</span>
+          <span className="text-2xl 2xl:text-4xl font-bold my-2 2xl:my-5">
+            153
+          </span>
           <span className="text-xs 2xl:text-sm tracking-wider text-white bg-black w-fit rounded-full px-2">
             +2%
           </span>
         </div>
-        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-64 2xl:w-80 h-36 2xl:h-40 p-4 flex flex-col">
+        <div className="rounded-xl border shadow-sm backdrop-blur-lg bg-white w-full h-36 2xl:h-44 p-4 2xl:pl-7 flex flex-col">
           <p className="text-lg 2xl:text-xl text-muted-foreground">
             Total Sales
           </p>
-          <span className="text-2xl 2xl:text-3xl font-bold my-2">329</span>
+          <span className="text-2xl 2xl:text-4xl font-bold my-2 2xl:my-5">
+            329
+          </span>
           <span className="text-xs 2xl:text-sm tracking-wider text-white bg-black w-fit rounded-full px-2">
             +8%
           </span>
