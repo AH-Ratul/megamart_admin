97ff5766992c666cddf7c143d8fa35d10e2d084d
AH-Ratul <ahratul21@gmail.com>
2024-11-08 21:19:44 +0600

    handle state for proudcts

---
+++ src/redux/slice/productSlice.js
@@ -0,0 +1,49 @@
+const { createSlice } = require("@reduxjs/toolkit");
+const { productsApi } = require("../api/product_api");
+
+const initialState = {
+  products: "",
+  loading: false,
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      productsApi.endpoints.addProducts.matchPending,
+      (state) => {
+        state.loading = true;
+      }
+    );
+    builder.addMatcher(
+      productsApi.endpoints.addProducts.matchFulfilled,
+      (state, action) => {
+        state.products = action.payload;
+        state.loading = false;
+      }
+    );
+    builder.addMatcher(
+      productsApi.endpoints.addProducts.matchRejected,
+      (state) => {
+        state.loading = false;
+      }
+    );
+    builder.addMatcher(
+      productsApi.endpoints.getProducts.matchPending,
+      (state) => {
+        state.loading = true;
+      }
+    );
+    builder.addMatcher(
+      productsApi.endpoints.getProducts.matchFulfilled,
+      (state, action) => {
+        state.products = action.payload;
+        state.loading = false;
+      }
+    );
+  },
+});
+
+export default productsSlice.reducer;
