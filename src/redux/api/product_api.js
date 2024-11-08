77445c2e3bacdb1e5b6ca6b21220aea67646b8c9
AH-Ratul <ahratul21@gmail.com>
2024-11-08 21:19:06 +0600

    handle api for products

---
+++ src/redux/api/product_api.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+
+export const productsApi = createApi({
+  reducerPath: "productsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_baseurl}/api/v1`,
+    credentials: true,
+  }),
+  tagTypes: ["products"],
+  endpoints: (builder) => ({
+    addProducts: builder.mutation({
+      query: (products) => ({
+        url: "/products/addProduct",
+        method: "POST",
+        body: products,
+      }),
+    }),
+    getProducts: builder.query({
+      query: "/products/getProducts",
+      method: "GET",
+    }),
+  }),
+});
