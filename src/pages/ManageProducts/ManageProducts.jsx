ba5701b2d459c0682d12cc29169960455078f1fa
AH-Ratul <ahratul21@gmail.com>
2024-12-11 12:35:24 +0600

    add new page for manage products

---
+++ src/pages/ManageProducts/ManageProducts.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ManageProducts = () => {
+  return (
+    <div className="flex justify-center items-center ml-5 mr-5 mt-[10px]">
+      <div className="w-full border bg-white p-8 rounded-sm">
+        <h1 className="text-3xl font-bold ">Manage Products</h1>
+        
+          
+      </div>
+    </div>
+  );
+};
+
+export default ManageProducts;
